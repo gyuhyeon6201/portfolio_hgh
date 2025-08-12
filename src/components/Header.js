9969a099bfad2012016e3a5bdcaaab8d361613f6
gyuhyeon6201 <rbgus303@gmail.com>
2025-08-12 12:34:20 +0900

    create Portfolio_HGH (0812)

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import { GoHomeFill } from "react-icons/go";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+// import { HiMenuAlt3 } from "react-icons/hi";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <GoHomeFill />
+      <HiOutlineMenuAlt3 />
+      {/* <HiMenuAlt3 /> */}
+    </div>
+  );
+};
+
+export default Header;
